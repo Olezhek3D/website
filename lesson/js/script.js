a5c7c588734ab1f71c26930bfbf4f5a395a6ee32
Oleg <oleg0kofficial@gmail.com>
2024-10-11 17:41:12 +0300

    I'm wanna do it public
    website about car

---
+++ lesson/js/script.js
@@ -0,0 +1,25 @@
+var data = {
+    "english":
+    {
+        "header__contact": "contacts",
+        "intro__title": "Car",
+        "intro__price": "Price - 49999.99$",
+        "intro__description": "Car for daily-driving!",
+        "intro__button": "BUY",
+        "reasons__title": "Advantages and disadvantages to have a car",
+        "reasons__dis": "Advantages",
+        "reasons_adv": "Disadvantages",
+        "reasons__item": "1 you're cool",
+        "reasons__item": "2 parents are proud of you",
+        "reasons__item": "4 independent",
+        "reasons__item": "5 infinite respect",
+        "reasons__item": "6 read it again",
+        "reasons__item": "1 none",
+        "reasons__item": "2 read the 1st",
+        "buy__title": "THANKS FOR ATTENTION!!!!!!",
+        "buy__text1": "so you gotta buy a car",
+        "buy__text2": " buy, you won't regret",
+        "footer__author": "author: Oleg Perevalov",
+        "footer__rights": "All rights reserved",
+    }
+}
